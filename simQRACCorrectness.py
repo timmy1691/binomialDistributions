2f965fc6cbc01c89e576f0604e6d0f9194d13272
Danning Zhan <danning.zhan@hotmail.com>
2026-03-03 15:00:44 +0100

    fixed binomials

--- simQRACCorrectness.py
+++ simQRACCorrectness.py
@@ -1,60 +1,98 @@
 import numpy as np
 from collections import Counter
-from matplotlib import pyplot as plt
 import math
 
 class Multinomial:
-    def __init__(self, p):
-        self.p = p
+    def __init__(self, p, numCats = None, desiredProb = None):
+        self.setProbs(p)
+        self.numCats = len(p)
 
-    
-    def simulatedMultinomialSampling(self, numSamples,desiredIndex = None, numTrials=1000):
+    def setProbs(self, p):
+        if isinstance(p, list):
+            self.p = max(p)
+            self.index = p.index(max(p))
+            self.q = (1 - self.p) / (len(p) - 1)
+        else:
+            self.p = p
+            self.q = (1 - p) / (self.numCats - 1)
+
+    def simulatedMultinomialSampling(self, numSamples, probVector, desiredIndex, numTrials=1000):
         """
         Sampling for emperical estimation of number of samples to form shape of distribution.
         input: probability vector
         """
 
         successCount = 0
-        if desiredIndex is None:
-            max_p = self.p.index(max(self.p))
-        else:
-            max_p = desiredIndex
 
-
-        for i in range(numTrials):
-            samples = np.random.choice(range(self.p), size=numSamples, p=self.p)
+        for _ in range(numTrials):
+            samples = np.random.choice(range(self.numCats), size=numSamples, p=probVector, replace=True)
             counts = Counter(samples)
-            most_common = counts.most_common(1)
+            most_common = counts.most_common(2)
             if len(most_common) < 2:
-                if len(most_common) == 1:
-                    if most_common[0][0] == max_p:
-                        successCount += 1
-                    continue
-
-            max_count = most_common[0][1]
-            second_max_count = most_common[1][1]
-            difference = max_count - second_max_count
-            if difference > 0:
-                successCount += 1
-        
+                # unique count from the sampler
+                if most_common[0][0] == desiredIndex:
+                    successCount += 1
+                
+            else:
+                # if two things are unique count
+                max_count = most_common[0][1]
+                second_max_count = most_common[1][1]
+                if most_common[0][0] == desiredIndex and max_count > second_max_count:
+                    successCount += 1
+                    
         return successCount / numTrials
     
 
-    def minSampleFinderSimulation(self, q,desiredIndex = None, numTrials=1000):
+    def minSampleFinderSimulation(self, q, desiredIndex = None, numTrials=1000):
         """
         Simulated method of finding minimum number of samples to achieve confidence level q
+
+        brute force find the emperical sample size
         """
 
         if desiredIndex is None:
-            desiredIndex = self.p.index(max(self.p))
-
+            probVector = [self.p] + [self.q] * (self.numCats - 1)
+            desiredIndex = 0
+        else:
+            if desiredIndex >= self.numCats:
+                raise ValueError("Desired index must be one of the categories")
+            else:
+                probVector = [self.q]*desiredIndex + [self.p] + [self.q]*(self.numCats - desiredIndex - 1)
+            
         numSamples = 1
         samplesRes = {}
         while True:
-            successRate = self.simulatedMultinomialSampling(numSamples, desiredIndex=desiredIndex, numTrials=numTrials)
+            successRate = self.simulatedMultinomialSampling(numSamples, probVector, desiredIndex, numTrials=numTrials)
             samplesRes[numSamples] = successRate
             if successRate >= q:
                 return numSamples
             numSamples += 1
 
+        
 
+
+
+    def binning(self, numSamples):
+        """
+        Number of samples is clear majority, then need to distribute the remaining samples
+        Number of ways to permute is given by (n + k - 1) C (k-1)
+        """    
+        numCats = self.numCats - 1
+        return math.comb(numSamples - 1, numCats - 1)
+
+
+    def violations(self, numSamples, maxCount):
+        """
+        Count the number of violating setups
+        """
+        numCats = self.numCats
+        
+        maxViolations = numSamples //maxCount 
+        if maxViolations >= numCats:
+            return -1
+        violationCount  = 0
+        for i in range(1, maxViolations + 1):
+            violationCount += math.comb(numCats, i) * (-1)**(i+1) * math.comb(numSamples-i*(maxCount) + numCats - 1, numCats - 1)
+        
+        return violationCount
+    
--- simsMessageCorrectness.py
+++ simsMessageCorrectness.py
@@ -1,24 +1,100 @@
 import numpy as np
 import math
-from matplotlib import pyplot as plt
 
 class Binomial:
-    def __init__(self, p):
-        self.p = p
-
-
-
-    def binomial(self, n):
+    def __init__(self, desiredProb, numSamples, probBlock=None):
         """
-        Binomail distribution to find the number of samples with success probability and result probability q
+        Input one probability, the over desired success probability 'DesiredProb"
+        Or the success prob of a single block blockProb
+        The number of samples in the binomial distribution numSamples
+     
         """
+        self.desiredProb = self.setProb(desiredProb)
+        self.numSamples = numSamples
+        self.probBlock = probBlock
 
+
+    def setProb(self, desiredProb):
+        if isinstance(desiredProb, float):
+            if desiredProb < 1 and desiredProb > 0:
+                return desiredProb
+    
+        raise ValueError("Desired probability must be a float between 0 and 1")
+        
+
+
+    def binomialSamples(self, n):
+        """
+        Binomail distribution probability for a fixed probability p and input sample size of n
+        """
+        if self.probBlock is not None:
+            p = self.probBlock
+        else:
+            raise ValueError("Block probability must be set to calculate binomial distribution")
         minK = n//2 + 1
         totalProbability = 0
         for k in range(minK, n+1):
             totalProbability += math.comb(n, k) * (self.p ** k) * ((1 - self.p) ** (n - k))
-        
         return totalProbability
+    
+    def binomialProb(self, p):
+        """
+        Calculate the total prob for binomial distribtuion for a fixed sample size n and success probability p
+        """
+
+        if p >= 1:
+            raise ValueError("Probability must be less than 1")
+        if p <= 0:
+            raise ValueError("Probability must be greater than 0")
+        
+        numSamples = self.numSamples
+        if numSamples % 2 == 0:
+            minK = numSamples//2 + 1
+        else:
+            minK = (numSamples + 1)//2
+
+        totalProbs = 0
+        for i in range(minK, numSamples+1):
+            totalProbs += math.comb(numSamples, i) * (p ** i) * ((1 - p) ** (numSamples - i))
+
+        return totalProbs
+    
+    def messageCorrectness(self):
+        """
+        Message Correctness is given by receiving each block correctly.
+        This means given a confideence in the message, one takes the nth root for n samples to get the confidence in each block.
+        """
+        if self.probBlock is None:
+            confidence = self.desiredProb
+            numSamples = self.numSamples
+            self.probBlock = confidence**(1/numSamples)
+            return self.probBlock
+
+    
+    def reverseBinomial(self):
+        """
+        Given a fixed sample, find the minimum probability to achieve confidence level q
+        """
+        q = self.desiredProb
+        n = self.numSamples
+
+        if n == 1:
+            return q
+        
+
+        if q > 0.5:
+            lowerP = 0.5
+            upperP = 1.0
+            while upperP - lowerP > 1e-6:
+                midP = (lowerP + upperP) / 2
+                if self.binomialProb(midP) <= q:
+                    lowerP = midP
+                else:
+                    upperP = midP
+
+        self.probBlock = midP    
+
+        return midP
 
 
     def naiveMinSamples(self, q):
@@ -28,10 +104,10 @@ class Binomial:
         Calculate the analytical expected value
         """
 
-        if self.p <= 0.5:
+        if self.probBlock <= 0.5:
             raise ValueError("Probability of success must be greater than 0.5")
         
-        if q <= self.p :
+        if q <= self.probBlock :
             return 1
 
         currentProb = 0
